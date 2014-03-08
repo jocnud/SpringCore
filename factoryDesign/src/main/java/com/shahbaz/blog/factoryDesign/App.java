8481d8829e9e22e8c8b0babdfbb0dfc485059690
jocnud@gmail.com <jocnud@gmail.com>
2014-03-08 18:32:41 +0530

    Testing repository

--- factoryDesign/src/main/java/com/shahbaz/blog/factoryDesign/App.java
+++ factoryDesign/src/main/java/com/shahbaz/blog/factoryDesign/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	
-    	System.out.println(" This is something  . . .   which can pass second test");
+    	System.out.println(" This is something  .  ONLINE COMMITING. .   which can pass second test");
     	
     	System.out.println("Implementing Factory Desin Patterns");
     	
